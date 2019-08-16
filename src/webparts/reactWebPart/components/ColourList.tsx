54b7e3a748ef135ff2b572965746130c0abcb98d
Krste Kovaceski <krste@pragdigi.com>
2019-08-16 16:19:52 +0200

    add buttons and change to fabric react

--- src/webparts/reactWebPart/components/ColourList.tsx
+++ src/webparts/reactWebPart/components/ColourList.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import {IColour} from '../IColour';
+import {List } from 'office-ui-fabric-react/lib/List';
+import { DefaultButton } from 'office-ui-fabric-react/lib/Button';
 
 export interface IColourListProps {
     colours: IColour[];
@@ -8,13 +10,21 @@ export interface IColourListProps {
 export class ColourList extends React.Component <IColourListProps, {}> {
 public render(): React.ReactElement<IColourListProps>{
     return(
-        <ul>
-            {
-                this.props.colours.map(colourItem => (
-                    <li>{ colourItem.title }</li>
-                ))
-            }
-        </ul>
+
+<List items={ this.props.colours }
+onRenderCell={ this._onRenderListCell }
+/>
     );
 }
+private _onRenderListCell = (colour: IColour, index: number | undefined ): JSX.Element => {
+
+    return ( <div>
+      { colour.title } <br />
+    <DefaultButton text="delete" data={colour.id} onClick={ () => this._onButtonClick(colour) } />  
+    </div>
+    );
+    }
+    private _onButtonClick(colour: IColour): void {
+        console.log('clicked delete for colour: ', colour);
+    }
 }
